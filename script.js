b9af7ae6fa6cab7aa20123768bb830a1e0ba8272
askamit2012 <askamit2012@hotmail.com>
2021-02-24 23:09:28 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+const shareButton = document.querySelector('button');
+        const overlay = document.querySelector('.overlay');
+        const shareModal = document.querySelector('.share');
+
+        const title = window.document.title;
+        const url = window.document.location.href;
+
+        shareButton.addEventListener('click', () => {
+            if (navigator.share) {
+                navigator.share({
+                    title,
+                    url
+                })
+                .then(() => console.log('Thanks for sharing!'))
+                .catch((e) => console.error(`Error: ${e}`));
+            } else {
+                overlay.classList.add('show-share');
+                shareModal.classList.add('show-share');
+            }
+        })
+
+        overlay.addEventListener('click', () => {
+            overlay.classList.remove('show-share');
+            shareModal.classList.remove('show-share');
+        })
